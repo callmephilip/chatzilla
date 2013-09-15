6e963c22565c190cd1dbbc1674a1045d1cf79ab4
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2013-09-16 01:52:53 +0200

    wire socket handlers

--- chatzilla.py
+++ chatzilla.py
@@ -1,5 +1,8 @@
 from gevent import monkey
-from flask import Flask
+from flask import Flask, Response, render_template, request
+from socketio import socketio_manage
+from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin
 
 monkey.patch_all()
 
@@ -8,6 +11,40 @@ application.debug = True
 application.config['PORT'] = 5000
 
 
+class ChatNamespace(BaseNamespace, BroadcastMixin):
+    def initialize(self):
+        self.logger = application.logger
+        self.log("Socketio session started")
+
+    def log(self, message):
+        self.logger.info("[{0}] {1}".format(self.socket.sessid, message))
+
+    def recv_connect(self):
+        self.log("New connection")
+
+    def recv_disconnect(self):
+        self.log("Client disconnected")
+
+    def on_join(self, name):
+        self.log("%s joined chat" % name)
+        return True, name
+
+    def on_message(self, message):
+        self.log('got a message: %s' % message)
+        self.broadcast_event_not_me("message", message)
+        return True, message
+
+
+
 @application.route('/', methods=['GET'])
 def landing():
-    return "Welcome to Chatzilla"
+    return render_template('landing.html')
+
+@application.route('/socket.io/<path:remaining>')
+def socketio(remaining):
+    try:
+        socketio_manage(request.environ, {'/chat': ChatNamespace}, request)
+    except:
+        application.logger.error("Exception while handling socketio connection",
+                         exc_info=True)
+    return Response()
